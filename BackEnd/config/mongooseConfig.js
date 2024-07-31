5d6d8d6d2038c7a8fadc5b4e65cb11385c50a65d
Cesar <cperezcampo30@gmail.com>
2024-07-30 19:26:33 -0500

    database

--- BackEnd/config/mongooseConfig.js
+++ BackEnd/config/mongooseConfig.js
@@ -1,6 +1,6 @@
 //Modularizacion de la conexion
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb://localhost:27017/prueba-mongodb")
+mongoose.connect("mongodb://database:27017/prueba-mongodb")
 
 export default mongoose;
